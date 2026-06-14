39aaefa38a35feb31de8ab03620f2c5c8096f020
0011taxidriver-pp <0011taxidriver@gmail.com>
2026-06-14 20:40:52 +0530

    Time: 0 ms (100.00%) | Memory: 128.3 MB (54.91%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>arr1;
-        vector<int>arr2;
-        vector<int>res;
+        vector<int>res(nums.size());
+        int pidx=0;
+        int nidx=1;
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]>0)
-            arr1.push_back(nums[i]);
-            else
-            arr2.push_back(nums[i]);
-        }
-        int i=0;
-        int j=0;
-        while(i<arr1.size()&&j<arr2.size())
-        {
-            res.push_back(arr1[i]);
-            i++;
-            res.push_back(arr2[j]);
-            j++;
+            {
+                res[pidx]=nums[i];
+                pidx=pidx+2;
+            }
+            else{
+                res[nidx]=nums[i];
+                nidx=nidx+2;
+            }
         }
         return res;
     }
